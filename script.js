52aa2b1adbe508e23a464a4d349a8ca487b74866
Ciarm-nik <nikcia@libero.it>
2021-04-19 17:00:05 +0200

    Inserito i due coupon funzionanti

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 var submitBnt = document.getElementById("button")
 
 submitBnt.addEventListener("click", function () {
+    // Metto un alert se l'utente non inserisce il nome del burger
+
 
     var nomeBurger = document.getElementById("name");
     var price = document.getElementById("price");
-    var coupon = document.getElementById("coupon");
+    var coupon = document.getElementById("coupon").value;
     var somma = 50
 
-    coupon = somma*0.10
+
+    if (!nomeBurger.value) {
+        alert("Manca il nome del burger")
+        return;
+    }
+
+    // coupon = ["1234frdnmi", "2342jfkkfr"]
+    // console.log(coupon[1])
 
     // Seleziono tutti gli elementi delle checkbox
     var listaIngredienti = document.querySelectorAll(".ingredient [type='checkbox']");
@@ -18,15 +27,23 @@ submitBnt.addEventListener("click", function () {
 
         if (ingrediente.checked) {
             somma += parseInt(ingrediente.value)
-        } 
+        }
     }
+
+    // Calcolo coupon
+    var sconto = 0;
+    // calcolo sconto in base a coupon
+    if (coupon == "1234frdnmi") {
+        sconto = somma * 0.10;
+    } else if (coupon == "2342jfkkfr") {
+        sconto = somma * 0.20;
+    } else {
+        sconto = 0
+    }
+
     // Stampo il prezzo finale
-    price.innerHTML = somma
+    price.innerHTML = somma - sconto
 
-    // Metto un alert se l'utente non inserisce il nome del burger
 
-    if (!nomeBurger.value) {
-        alert("Manca il nome del burger")
-    }
 })
 
